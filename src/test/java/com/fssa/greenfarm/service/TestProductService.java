dff1e4543cf564bf406227fc1bb19bacfc6c3c1a
prathiusha22 <prathiusha22@gmail.com>
2023-09-06 11:36:58 +0530

    alter the java files

--- src/test/java/com/fssa/greenfarm/service/TestProductService.java
+++ src/test/java/com/fssa/greenfarm/service/TestProductService.java
@@ -20,7 +20,7 @@ public class TestProductService {
 	@Test
 	void testAddProduct() {
 		// Create a sample product for testing
-		Product product = new Product("Tomato", 15,"https://iili.io/.jpg", 50, 2, 50,
+		Product product = new Product("Tomato", 17,"https://iili.io/.jpg", 50, 2, 50,
 				"It is good in vitamins and proteins and carbohydrates", "vegetable", LocalDate.of(2023, 07, 10));
 		ProductService productservice = new ProductService();
 
