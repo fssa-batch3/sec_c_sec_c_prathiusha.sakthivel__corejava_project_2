83d0f8abf5d15dc3b08c74a46d7ea4502ae02d3a
prathiusha22 <116252266+prathiusha22@users.noreply.github.com>
2023-09-06 21:54:15 +0530

    commited after clearing sonarcloud bugs

--- src/main/java/com/fssa/greenfarm/DAO/ProductConnection.java
+++ src/main/java/com/fssa/greenfarm/DAO/ProductConnection.java
@@ -1,51 +1,48 @@
 package com.fssa.greenfarm.DAO;
-
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
 
 import com.fssa.greenfarm.exception.DAOException;
 import com.fssa.greenfarm.logger.Logger;
 
 public class ProductConnection {
-	private ProductConnection() {
-		// Private constructor to prevent instantiation
-	}
+    private ProductConnection() {
+        // Private constructor to prevent instantiation
+    }
 
-	/**
-	 * Get a connection to the database.
-	 * 
-	 * @return The database connection.
-	 * @throws DAOException
-	 * @throws RuntimeException if unable to connect to the database.
-	 */
-	public static Connection getConnection() throws DAOException {
+    /**
+     * Get a connection to the database.
+     * 
+     * @return The database connection.
+     * @throws DAOException
+     * @throws RuntimeException if unable to connect to the database.
+     */
+    public static Connection getConnection() throws DAOException {
+        String url;
+        String userName;
+        String passWord;
+        Connection con = null;
+        url = System.getenv("DATABASE_HOST");
+        userName = System.getenv("DATABASE_USERNAME");
+        passWord = System.getenv("DATABASE_PASSWORD");
+        
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            con = DriverManager.getConnection(url, userName, passWord);
+            Logger.info("Connection success");
+        } catch (Exception e) {
+            throw new DAOException("Unable to connect to the database");
+        }
+        
+        return con;
+    }
 
-		String url;
-		String userName;
-		String passWord;
-		Connection con = null;
-
-		url = System.getenv("DATABASE_HOST");
-		userName = System.getenv("DATABASE_USERNAME");
-		passWord = System.getenv("DATABASE_PASSWORD");
-
-		try {//
-			Class.forName("com.mysql.cj.jdbc.Driver");
-			con = DriverManager.getConnection(url, userName, passWord);
-			Logger.info("Connection success");
-		} catch (Exception e) {
-			throw new DAOException("Unable to connect to the database");
-		}
-		return con;
-
-	}
-
-	public static void main(String[] args) {
-
-		try {
-			getConnection();
-		} catch (DAOException e) {
-			e.printStackTrace();
-		}
-	}
+    public static void main(String[] args) throws SQLException {
+        try (Connection connection = getConnection()) {
+            // Use the connection here if needed
+        } catch (DAOException e) {
+            e.printStackTrace();
+        }
+    }
 }
