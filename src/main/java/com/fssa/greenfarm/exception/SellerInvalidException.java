fa8f041ff75dc6425ca6960a564e35a382119639
PrathiushaSakthivel <116252266+prathiusha22@users.noreply.github.com>
2024-01-17 11:31:12 +0530

    Delete src/main/java/com/fssa/greenfarm/exception/SellerInvalidException.java

--- src/main/java/com/fssa/greenfarm/exception/SellerInvalidException.java
+++
@@ -1,9 +0,0 @@
-package com.fssa.greenfarm.exception;
-
-public class SellerInvalidException extends Exception{
-	private static final long serialVersionUID = 1L;
-
-	public SellerInvalidException(String msg) {
-		super(msg);
-	}
-}
